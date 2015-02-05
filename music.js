47edcbd890241f32c771da339d59f809aa875792
Han Chen <thechenhan@gmail.com>
2015-02-04 22:15:32 -0500

    change to music box. let's try

--- music.js
+++ music.js
@@ -8,27 +8,27 @@ var player; //全局midi播放器
 var clock = new THREE.Clock();
 
 
-// window.onload = function() {
-// //load the music_box soundfont and change the instrument to music_box.
-//   MIDI.loadPlugin({
-//     soundfontUrl: "/soundfont/",
-//     instruments: "music_box",
-//     callback: function() {
-//  MIDI.programChange(0, MIDI.GeneralMIDI.byName["music_box"].number);
-//     }  
-//   });
-// }
-
-
 window.onload = function() {
-  MIDI.loadPlugin(function() {
-    console.log("Sound being generated with " + MIDI.lang + ".");
-    
-    if (window.location.hash === '#' || window.location.hash === '') {
-      //switchTo('tracks/157-Rachmaninov - Flight of the Bumblebee');
-    }
-  }, "soundfont/acoustic_grand_piano-mp3.js");
+//load the music_box soundfont and change the instrument to music_box.
+  MIDI.loadPlugin({
+    soundfontUrl: "/soundfont/",
+    instruments: "music_box",
+    callback: function() {
+ MIDI.programChange(0, MIDI.GeneralMIDI.byName["music_box"].number);
+    }  
+  });
 }
+
+
+// window.onload = function() {
+//   MIDI.loadPlugin(function() {
+//     console.log("Sound being generated with " + MIDI.lang + ".");
+    
+//     if (window.location.hash === '#' || window.location.hash === '') {
+//       //switchTo('tracks/157-Rachmaninov - Flight of the Bumblebee');
+//     }
+//   }, "soundfont/acoustic_grand_piano-mp3.js");
+// }
 
 
 
